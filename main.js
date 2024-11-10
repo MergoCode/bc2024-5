5e785dec84a74c4348a75f063ee9bce22659a752
Нікітов Віктор bc2024-5 <gen.nishant.1402@gmail.com@>
2024-11-10 22:37:35 +0200

    Added POST

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { Command } = require('commander');
 const path = require('path');
 const fs = require('fs');
+const exp = require("constants");
  
 const program = new Command();
 
@@ -21,6 +22,7 @@ if (!fs.existsSync(cacheDirectory)) {
 }
 
 const app = express();
+app.use(express.json());
 
 app.get('/',(req, res) => {
     res.send("Hello world!");
@@ -50,5 +52,29 @@ app.get(`/notes/:name`, (req, res) => {
     }
     
     res.send(note.text);
+});
+
+app.post('/notes/write', (req, res) => {
+    const { name } = req.body; 
+
+    if (!name) {
+        return res.status(400).send("Name parameter is required.");
+    }
+
+    console.log(`Received request to create note with name: ${name}`);
+
+    const note = notes.find(note => note.name === name);
+
+    if (note) {
+        return res.status(409).send("Note already exists!");
+    }
+
+    const newNote = {
+        name: name,
+        text: req.body.text || "No text provided" 
+    };
+
+    notes.push(newNote);
+    return res.status(201).send(newNote);
 });
 
